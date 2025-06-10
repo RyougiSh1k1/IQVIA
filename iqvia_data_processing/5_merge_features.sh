ca5e6aee4b46a900fe5114a3dd37753040050988
RyougiSh1k1 <734714416@qq.com>
2025-06-10 13:28:48 -0700

    3

---
+++ iqvia_data_processing/5_merge_features.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=5_merge_features
+#SBATCH --output=5_merge_features.out
+#SBATCH --error=5_merge_features.err
+
+srun python 5_merge_features.py > 5_merge_features.log
